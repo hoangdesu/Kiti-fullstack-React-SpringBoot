130a867a4b49a3c187e6b76d939a396c49a33a47
hoangdesu <hoangdesu@gmail.com>
2021-11-25 13:14:56 +0700

    Project created

---
+++ server/src/test/java/com/kiti/server/KitiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiti.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
